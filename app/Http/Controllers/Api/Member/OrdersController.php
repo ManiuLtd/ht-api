d44afea328f4ce14bb827ea9da9eaf90a71c51a1
niugengyun <515343908@qq.com>
2018-10-07 16:24:20 +0800

    前端控制器

---
+++ app/Http/Controllers/Api/Member/OrdersController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niugengyun
+ * Date: 2018/10/7
+ * Time: 16:18
+ */
+
+namespace App\Http\Controllers\Api\Member;
+
+
+use App\Http\Controllers\Controller;
+
+/**
+ * 订单管理
+ * Class OrdersController
+ * @package App\Http\Controllers\Api\Member
+ */
+class OrdersController extends Controller
+{
+
+    /**
+     * OrdersController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    //TODO 订单列表 可根据平台、时间查看
+    public function index()
+    {
+
+    }
+}
