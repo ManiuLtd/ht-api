dbea0528eb519ad401497c45898d429e37694f9c
123456 <137405213@qq.com>
2018-10-10 11:56:56 +0800

    更新

---
+++ app/Validators/Taoke/MemberOrderValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Validators\Taoke;
+
+use Prettus\Validator\LaravelValidator;
+use Prettus\Validator\Contracts\ValidatorInterface;
+
+/**
+ * Class MemberOrderValidator
+ * @package App\Validators\Taoke
+ */
+class MemberOrderValidator extends LaravelValidator
+{
+    /**
+     * Validation Rules.
+     *
+     * @var array
+     */
+    protected $rules = [
+        ValidatorInterface::RULE_CREATE => [],
+//        ValidatorInterface::RULE_UPDATE => [],
+    ];
+}
