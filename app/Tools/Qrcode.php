ced1032caabec80f0b72dfde0ace7cdd1dff2b4e
niugengyun <515343908@qq.com>
2018-10-10 18:27:37 +0800

    二维码

--- app/Tools/Qrcode.php
+++ app/Tools/Qrcode.php
@@ -10,13 +10,70 @@ namespace App\Tools;
 
 class Qrcode
 {
-    //TODO 邀请二维码
-    public function inviter()
+
+    /**
+     * @var string 模板路径
+     */
+    protected $templatePath;
+
+    /**
+     * @var  array 所有文字数组
+     */
+    protected $textArray;
+
+    /**
+     * @var  array 所有图片数组
+     */
+    protected $imageArray;
+
+    /**
+     * @param array $textArray
+     */
+    public function setTextArray(array $textArray): void
     {
+        $this->textArray = $textArray;
     }
 
-    //TODO 产品二维码
-    public function goods()
+    /**
+     * @param array $imageArray
+     */
+    public function setImageArray(array $imageArray): void
     {
+        $this->imageArray = $imageArray;
+    }
+
+
+    /**
+     * @param string $templatePath
+     */
+    public function setTemplatePath(string $templatePath): void
+    {
+        $this->templatePath = $templatePath;
+    }
+
+
+
+    public function generate()
+    {
+        $template = Image::make($this->templatePath);
+
+        if (count ($this->imageArray)){
+            foreach ($this->imageArray as $imgArr){
+                $template->insert($imgArr['image'], $imgArr['position'], $imgArr['x'], $imgArr['y']);
+            }
+        }
+        if (count ($this->textArray)){
+            foreach ($this->textArray as $textArr){
+                $template->text($imgArr['text'], 215, 1485, function ($font) use ($textArr){
+                    $font->file(public_path('fonts/yahei.ttf'));
+                    $textArr['size'] ?? $font->size($textArr['size']);
+                    $font->color('#9b9b9b');
+                    $font->valign('left');
+                });
+
+                $template->insert($imgArr['text'], $imgArr['position'], $imgArr['x'], $imgArr['y']);
+            }
+        }
+      //储存图片 返回
     }
 }
