c47d09723951df08b58faaebeb5aa6d3bc341302
niugengyun <515343908@qq.com>
2018-10-13 16:44:02 +0800

    分享图

---
+++ app/Criteria/OrderTypeCriteria.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Criteria;
+
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class OrderTypeCriteria.
+ *
+ * @package namespace App\Criteria;
+ */
+class OrderTypeCriteria implements CriteriaInterface
+{
+    /**
+     * Apply criteria in query repository
+     *
+     * @param string              $model
+     * @param RepositoryInterface $repository
+     *
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        //todo 根据type判断  1直推订单 2下下级订单 3团队订单 4补贴订单
+        return $model;
+    }
+}
