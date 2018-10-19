0ed1fd3c53eb421bd04c2c71e29004417bc88975
caojiannan <1129585272@qq.com>
2018-10-19 10:24:19 +0800

    快抢商品后台

---
+++ app/Validators/Taoke/KuaiqiangValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Validators\Taoke;
+
+use Prettus\Validator\LaravelValidator;
+use Prettus\Validator\Contracts\ValidatorInterface;
+
+/**
+ * Class KuaiqiangValidator.
+ */
+class KuaiqiangValidator extends LaravelValidator
+{
+    /**
+     * Validation Rules.
+     *
+     * @var array
+     */
+    protected $rules = [
+
+    ];
+    protected $messages = [
+
+    ];
+}
