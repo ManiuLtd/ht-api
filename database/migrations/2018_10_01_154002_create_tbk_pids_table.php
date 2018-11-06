7ef5dfa7fe6ab33cb82d763bed8c96ed29f1d584
wangu7 <tuplelist@163.com>
2018-11-06 10:27:34 +0800

    pull

--- database/migrations/2018_10_01_154002_create_tbk_pids_table.php
+++ database/migrations/2018_10_01_154002_create_tbk_pids_table.php
@@ -21,7 +21,7 @@ class CreateTbkPidsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->nullable();
             $table->integer('member_id')->nullable();
-            $table->integer('oauth_id')->nullable();
+
             $table->string('taobao', 100)->nullable();
             $table->string('jingdong', 100)->nullable();
             $table->string('pinduoduo', 100)->nullable();
