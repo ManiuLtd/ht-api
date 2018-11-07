de04c7e571183d4208f04508a73da20876118875
purpleve <18697310620>
2018-11-07 15:38:32 +0800

    修改

--- app/Repositories/Taoke/JingXuanRepositoryEloquent.php
+++ app/Repositories/Taoke/JingXuanRepositoryEloquent.php
@@ -66,7 +66,7 @@ class JingXuanRepositoryEloquent extends BaseRepository implements JingXuanRepos
             $tool = new Taobao();
             $command = $tool->getDetail([
                 'id' => $v['itemid']
-            ]);
+            ]); 
             if (strpos($v['comment1'],'$淘口令$')){
                 $model['data'][$k]['comment1'] = str_replace('$淘口令$',$command->kouling,$v['comment1']);
             }
