da8c480479ae8ab37158978127c19a8835594360
wangu7 <tuplelist@163.com>
2018-11-16 18:03:58 +0800

    login

--- database/migrations/2018_10_01_151244_create_tbk_categories_table.php
+++ database/migrations/2018_10_01_151244_create_tbk_categories_table.php
@@ -22,9 +22,8 @@ class CreateTbkCategoriesTable extends Migration
             $table->integer('user_id')->nullable();
             $table->string('name', 191)->nullable();
             $table->string('logo', 191)->nullable();
-            $table->string('taobao', 100)->nullable();
-            $table->string('jingdong', 100)->nullable();
-            $table->string('pinduoduo', 100)->nullable();
+            $table->tinyInteger('type')->nullable();
+            $table->integer('cat')->nullable();
             $table->integer('sort')->nullable()->default(100);
             $table->tinyInteger('status')->nullable()->default(1);
             $table->nullableTimestamps();
