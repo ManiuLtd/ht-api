5c73d4c6f6d398acdbfad373a291942b0e915878
niugengyun <515343908@qq.com>
2018-11-21 13:52:52 +0800

    修改等级

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ Route::middleware('web')->namespace('Frontend')->group(function () {
 Route::middleware('web')->prefix('wechat')->namespace('Wechat')->group(function () {
     Route::any('mini_program/serve', 'MiniProgramController@serve');
     Route::any('official_account/serve', 'OfficialAccountController@serve');
+    Route::any('official_account/login', 'OfficialAccountController@login');
 });
