54b1417df1803954b12dfb7238cbfc0e577b1551
niugengyun <515343908@qq.com>
2018-11-21 19:44:34 +0800

    H5

--- app/Http/Controllers/Wechat/OfficialAccountController.php
+++ app/Http/Controllers/Wechat/OfficialAccountController.php
@@ -91,7 +91,7 @@ class OfficialAccountController extends Controller
 
             $user = $app->oauth->user ();
 
-            dd ($user,request ('reditect_url'));
+            dd (request ('redirect_url'));
             $original = $user->getOriginal ();
 
             $user = User::query ()->where ([
