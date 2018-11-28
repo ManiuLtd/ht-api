ee55a6fa55fce6f9e9644acd2ed2ab5b3a0ccfb3
niugengyun <515343908@qq.com>
2018-11-28 18:23:57 +0800

    支付记录

--- database/migrations/2018_11_28_180929_create_user_payment_table.php
+++ database/migrations/2018_11_28_180929_create_user_payment_table.php
@@ -13,17 +13,16 @@ class CreateUserPaymentTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_payment', function (Blueprint $table) {
+        Schema::create('user_payments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->nullable()->comment('用户id');
             $table->decimal('price', 9, 2)->nullable()->comment('下单金额');
             $table->string('transaction_id')->nullable()->comment('微信支付订单号');
             $table->string('out_trade_no')->nullable()->comment('商户订单号');
-            $table->timestamp('time_end')->nullable()->comment('支付完成时间');
-            $table->timestamp('time_start')->nullable()->comment('下单时间');
-            $table->integer('type')->nullable()->comment('1 是等级 2 是订单');
-            $table->integer('status')->nullable()->comment('1 支付完成 2 支付未完成');
+            $table->tinyInteger('type')->nullable()->comment('1 是等级 2是订单');
+            $table->tinyInteger('status')->nullable()->comment('1 支付完成 2 支付未完成 3已退款');
             $table->string('other')->nullable()->comment('其余参数');
+            $table->timestamp('payment_at')->nullable()->comment('支付完成时间');
             $table->timestamps();
         });
     }
