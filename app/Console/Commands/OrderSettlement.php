eac4cccde2146a947782f372f628d2a8622c9ac5
niugengyun <515343908@qq.com>
2018-12-13 14:11:25 +0800

    bug

--- app/Console/Commands/OrderSettlement.php
+++ app/Console/Commands/OrderSettlement.php
@@ -44,6 +44,9 @@ class OrderSettlement extends Command
             ->whereDate('complete_at',date('Y-m'))
             ->get()
             ->toArray();
+
+        //FIXME 订单结算返佣哪里体现？ 订单是每月同时结算的，这样的订单比较多，应该用队列处理；下面的返还积分同理
+        //改为队列：creditorder  返利
         if (count($order) > 0){
             foreach ($order as $v){
                 event(new CreditOrderFriend([
